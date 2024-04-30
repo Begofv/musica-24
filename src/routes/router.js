9cc28cbe820b93fc44ada2963d8d7b79a4a8738a
bego <begofv13@gmail.com>
2024-04-30 15:30:21 +0200

    band

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,10 @@ import { Router } from "express";
 
 import apiRouter from "./apiRoutes/apiRouter.js";
 import viewRouter from "./viewRoutes/viewRouter.js";
+import artistRouter from "./artistRouter.js";
+import bandRouter from "./bandRouter.js";
+
+
 
 const router = Router();
 
@@ -10,5 +14,31 @@ const router = Router();
 router.use("/api",apiRouter);
 router.use("/",viewRouter);
 
+router.get("/",(req,res)=>{
+    res.send("Hello " + process.env.APP_USERNAME)
+})
+
+router.get("/search",(req,res)=>{
+    //const query = req.query.q;
+    const {q,name} = req.query;
+    res.send(`Mostrando resultados de la búsqueda '${q}' con el nombre ${name}`);
+})
+
+router.get("/form",(req,res)=>{
+    const formString = `
+    <form action="/search" method="get">
+        <label for="q">pregunta</label>
+        <input type="text" name="q" id="q">
+        <button type="submit">enviar</button>
+    </form> 
+    `
+    res.send(formString);   
+})
+
+router.use("/artist",artistRouter);
+router.use("/band",bandRouter);
+/* router.post("/search",(req,res)=>{
+    const {}
+}) */
 
 export default router;
